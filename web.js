afe7c9625a3582cece7299736e51bec9d6169bc1
Krishna M <m.krishna@live.in>
2013-07-10 18:28:11 +0000

    new web.js

--- web.js
+++ web.js
@@ -4,8 +4,10 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 	var fs = require('fs');
+	var b = new Buffer(30);
 	var k = fs.readFileSync('index.html','utf-8');
-	response.send(k);
+	b.write(k, "utf-8");
+	response.send(b.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
